236dde20756cffc881bfd0e5d6d742bcdc692d3a
Ruimin Shi <srm1120@outlook.com>
2021-09-07 04:47:21 +0000

    remove max min statistics

--- datahub-web-react/src/app/entity/shared/tabs/Dataset/Schema/utils/useStatisticsRenderer.tsx
+++ datahub-web-react/src/app/entity/shared/tabs/Dataset/Schema/utils/useStatisticsRenderer.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import { Row, Col } from 'antd';
 
-export default function useStatisticsRenderer() {
-    const nullPercent = Math.floor(Math.random() * (99 - 1 + 1) + 1);
-    const maxValue = Math.random().toFixed(2);
-    const minValue = (Number(maxValue) * Math.random()).toFixed(2);
-
+export default function useStatisticsRenderer(nullPercent: number) {
     const statisticsRenderer = () => {
         return (
             <>
                 <Row>
                     <Col span={12}>Null Percentage</Col>
                     <Col span={12}>{nullPercent}%</Col>
-                </Row>
-                <Row>
-                    <Col span={12}>Maximum</Col>
-                    <Col span={12}>{maxValue}</Col>
-                </Row>
-                <Row>
-                    <Col span={12}>Minimum</Col>
-                    <Col span={12}>{minValue}</Col>
                 </Row>
             </>
         );
